44e7d36e97e0ae34fe03c9d3bea98f36cee45563
T-Tavares <thiago.tavares.freire@gmail.com>
2024-12-10 00:20:33 +1300

    _ Challenge Solution

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,10 @@ function App() {
         <>
             <Header />
             <div className="movies-gallery">
-                <MovieCard
-                    title={database[1].title}
-                    genre={database[1].genre}
-                    poster={database[1].poster}
-                    runTime={database[1].runTime}
-                />
-                <MovieCard
-                    title={database[7].title}
-                    genre={database[7].genre}
-                    poster={database[7].poster}
-                    runTime={database[7].runTime}
-                />
-                <MovieCard
-                    title={database[20].title}
-                    genre={database[20].genre}
-                    poster={database[20].poster}
-                    runTime={database[20].runTime}
-                />
-                <MovieCard
-                    title={database[10].title}
-                    genre={database[10].genre}
-                    poster={database[10].poster}
-                    runTime={database[10].runTime}
-                />
-                <MovieCard
-                    title={database[5].title}
-                    genre={database[5].genre}
-                    poster={database[5].poster}
-                    runTime={database[5].runTime}
-                />
+                {database.map(movie => {
+                    const {title, genre, runTime, poster} = movie;
+                    return <MovieCard key={movie.id} title={title} genre={genre} runTime={runTime} poster={poster} />;
+                })}
             </div>
         </>
     );
