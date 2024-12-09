459092e56cc12498d9d0f4ff971c93309591b454
T-Tavares <thiago.tavares.freire@gmail.com>
2024-12-10 01:15:39 +1300

    Start Prep

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
-import database from './database/database';
 
 function App() {
     return (
         <>
             <Header />
-            <div className="movies-gallery">
-                {database.map((movie, index) => {
-                    const {title, genre, runTime, poster} = movie;
-                    return (
-                        <MovieCard
-                            key={movie.title + index}
-                            title={title}
-                            genre={genre}
-                            runTime={runTime}
-                            poster={poster}
-                        />
-                    );
-                })}
-            </div>
+            <div className="movies-gallery"></div>
         </>
     );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import './MovieCard.css';
 
-function MovieCard({poster, title, runTime, genre}) {
+function MovieCard() {
     return (
         <div className="movie-card">
-            <img className="bg-img" src={poster} alt="" />
-            <div className="info">
-                <p className="title">{title}</p>
-                <p className="duration"> Duration: {runTime}</p>
-                <p className="genre">{genre.join(' - ')} </p>
+            <img className="bg-img" src="https://dummyimage.com/720x1280/777/aaa" alt="" />
+            <div className="">
+                <p className="title">title</p>
+                <p className="duration"> Duration: </p>
+                <p className="genre"> </p>
             </div>
         </div>
     );
